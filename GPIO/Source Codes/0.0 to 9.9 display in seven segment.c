ec8fc51ae009d07fb27c2c87da2000f4a5866d12
Vishwa <vishwa2439@gmail.com>
2026-01-31 08:55:13 +0530

    Practice Codes

---
+++ GPIO/Source Codes/0.0 to 9.9 display in seven segment.c
@@ -0,0 +1,27 @@
+// use #include "Vishwa.h"
+int main()
+{
+  unsigned int c=0;
+  portf(0xff);
+  portk(0x03);
+    while(1)
+  {
+    for(int i=0;i<10;i++)
+    {
+      for(int j=0;j<10;j++)
+      {
+        c=0;
+        while(c<50000)
+        {
+          segdot(i);
+      highlowk(0x02);
+      highlowk(0xff);
+     seg(j);
+     highlowk(0x01);
+     highlowk(0xff);
+     c++;
+        }
+      }
+  }
+}
+}
---
+++ GPIO/Source Codes/Lift using 7segment.c
@@ -0,0 +1,87 @@
+// use  #include "Header1.h"
+int main()
+{
+  int a=0;
+  portf(0xff);
+  portk(0x00);
+  highlowk(0xff);
+  int b=0;
+  while(1)
+  {
+    b=portkread();
+  if(b==0xf7)
+{
+  seg(1);
+    if(a<3)
+    {
+    for(int i=a;i<=3;i++)
+    {
+      seg(i);
+      delay1(6);
+    }
+    }
+    seg(3);
+    a=3;
+  }
+   if(b==0xfb)
+  {
+    if(a<2)
+    {
+    for(int i=a;i<=2;i++)
+    {
+      seg(i);
+      delay1(6);
+
+    }
+    }
+    if(a>2)
+    {
+      for(int i=3;i>=3;i--)
+      {
+        seg(i);
+        delay1(6);
+
+      }
+    }
+    seg(2);
+    a=2;
+  }
+  if(b==0xfd)
+  {
+    if(a>1)
+    {
+      for(int i=a;i>=1;i--)
+      {
+        seg(i);
+        delay1(6);
+
+      }
+    }
+    if(a<1)
+    {
+      for(int i=0;i<=1;i++)
+      {
+        seg(i);
+        delay1(6);
+
+      }
+    }
+    seg(1);
+    a=1;
+  }
+  if(b==0xfe)
+  {
+    if(a>0)
+    {
+      for(int i=a;i>=0;i--)
+      {
+        seg(i);
+        delay1(6);
+
+      }
+    }
+    seg(1);
+    a=1;
+  }
+  }
+}
